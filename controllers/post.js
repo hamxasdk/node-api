04b224873126c57d73d895d5928ece3551df0538
Hamza Ashraf <hamzamirza658@gmail.com>
2022-07-26 17:44:32 +0500

    mongo-integration

--- controllers/post.js
+++ controllers/post.js
@@ -1,3 +1,19 @@
+const Post = require('../models/post')
 exports.getPosts = (req, res) => {
-  res.send('A Node JS API From Scratch !!!')
+  res.json({
+    posts: [{ title: 'First Post' }, { title: '2nd Post' }],
+  })
+}
+exports.createPost = (req, res) => {
+  const post = new Post(req.body)
+  post.save((err, result) => {
+    if (err) {
+      return res.status(400).json({
+        error: err,
+      })
+    }
+    res.status(200).json({
+      post: result,
+    })
+  })
 }
--- index.js
+++ index.js
@@ -2,15 +2,21 @@ const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const postRoutes = require('./routes/post')
-
-// const myOwnMiddleWare = (req, res, next) => {
-//   console.log('MiddleWare Applied !!')
-//   next()
-// }
+const dotenv = require('dotenv')
+const mongoose = require('mongoose')
+const bodyParser = require('body-parser')
+dotenv.config()
+// connection with db
+mongoose.connect(process.env.MONGO_URI).then(() => {
+  console.log('DB is connected')
+})
+mongoose.connection.on('error', (err) => {
+  console.log('Db is not Connected', err.message)
+})
 //middleware
 app.use(morgan('dev'))
-// app.use(myOwnMiddleWare)
-
+app.use(bodyParser.json())
 app.use('/', postRoutes)
-const port = 4000
-app.listen(port, () => console.log('Api is Running on Port :', port))
+const PORT = process.env.PORT || 4000
+
+app.listen(PORT, () => console.log('Api is Running on Port :', PORT))
